9db72de520e39693b864a98c54b9a6cc6954d196
Bruce Cain <brucecainjr@gmail.com>
2018-06-14 16:06:43 -0400

    Initial creation of the Project

---
+++ bottles.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+# Project 1
+# 99 Bottles
+#
+# UMBC TDQC5
+# Bruce Cain
+#
+# Creation of the 99 Bottles on the wall song
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
